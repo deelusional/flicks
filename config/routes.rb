55520734b101e8eab8c35044ffb75abd4a3ae4d9
deelusional <dtapmalai@gmail.com>
2015-05-06 15:33:02 -0700

    Added create new movie page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   root "movies#index"
-  get  "movies" => "movies#index"
-  get  "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+
+  # get  "movies" => "movies#index"
+  # get "movies/new" => 'movies#new'
+  # get  "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # patch "movies/:id" => "movies#update"
+
+  resources :movies
 
 
   # The priority is based upon order of creation: first created -> highest priority.
